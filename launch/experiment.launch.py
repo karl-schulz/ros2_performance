33ad9f3b5580c419ad5ff79b6cc81208411d20c6
Karl Schulz <karl@angsa-robotics.com>
2022-09-06 10:41:02 +0200

    Launch files for launching pub and sub together

---
+++ launch/experiment.launch.py
@@ -0,0 +1,37 @@
+from launch.substitutions import LaunchConfiguration
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    conf_mb = LaunchConfiguration("mb")
+    conf_hz = LaunchConfiguration("hz")
+
+    pub = Node(
+        name="pub",
+        executable="publisher",
+        package="ros2_performance",
+        parameters=[{
+            "mb": conf_mb,
+            "hz": conf_hz,
+        }],
+        output="screen",
+    )
+    sub_0 = Node(
+        name="sub_0",
+        executable="subscriber",
+        package="ros2_performance",
+        output="screen",
+    )
+    sub_1 = Node(
+        name="sub_1",
+        executable="subscriber",
+        package="ros2_performance",
+        output="screen",
+    )
+    return LaunchDescription([
+        pub,
+        sub_0,
+        sub_1,
+    ])
